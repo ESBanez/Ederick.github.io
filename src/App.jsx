96884e86659087b30a1168a5dd60dcdd6950e334
Ederick S. Banez <esb.tailor@gmail.com>
2023-10-25 19:35:45 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Skills from "./pages/Skills";
+import Pricing from "./pages/Pricing";
+import Blog from "./pages/Blog";
+
+import CurriculumVitae from "./pages/settings/CurriculumVitae";
+import Profile from "./pages/settings/Profile";
+import Projects from "./pages/settings/Projects";
+
+
+
+import Navbar from "./components/Navbar";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#040D12', // Replace '#f00' with your desired color value
+    },
+  },
+})
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/curriculum-vitae" element={<CurriculumVitae />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/projects" element={<Projects />} />
+
+      </Routes>
+      </ThemeProvider>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
