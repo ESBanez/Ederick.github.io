472c1899523ad882047c7fa201fb19c51e31128a
Ederick S. Banez <esb.tailor@gmail.com>
2024-01-26 17:36:23 +0800

    fix the skillsCarousel and add sticky

--- src/components/SkillsCarousel.jsx
+++ src/components/SkillsCarousel.jsx
@@ -1,154 +1,70 @@
-import '../sass/SkillsCarousel.scss'; // Make sure to adjust the path to your actual stylesheet
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import * as React from 'react';
+import '../sass/SkillsCarousel.scss';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
 
+// import Typography from '@mui/material/Typography';
+// import { Button, CardActionArea, CardActions } from '@mui/material';
+// import CardContent from '@mui/material/CardContent';
 
-
-const styles = {
-  neon: '#00FFA1',
-  blacky: '#040D12',
-  darkgreen: '#183D3D',
-  blue: '#000F1A',
-  green: '#5C8374',
-  grey: '#93B1A6',
-};
-
-
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-    '& .MuiDialogContent-root': {
-      padding: theme.spacing(2),
-    },
-    '& .MuiDialogActions-root': {
-      padding: theme.spacing(2),
-    },
-  }));
-  
 
 function SkillsCarousel() {
-    const [open, setOpen] = React.useState(false);
-    const [selectedImage, setSelectedImage] = React.useState(null);
-  
-    const handleClickOpen = (image) => {
-      setSelectedImage(image);
-      setOpen(true);
-    };
-  
-    const handleClose = () => {
-      setOpen(false);
-    };
-
   const numImages = [
     {
-      title: "Front End",
+      title: 'Front End',
       index: 0,
-      dialoge: "I learned about Front End in Bootcamp",
-      src: "/1.png",
+      dialoge: 'I learned about Front End in Bootcamp',
+      src: '/1.png',
     },
     {
-      title: "Back End",
+      title: 'Back End',
       index: 1,
-      dialoge: "I learned about Back End in Bootcamp",
-      src: "/2.png",
+      dialoge: 'I learned about Back End in Bootcamp',
+      src: '/2.png',
     },
     {
-      title: "Logo Maker",
+      title: 'Logo Maker',
       index: 2,
-      dialoge: "I learned about Logo Maker in through exposure in creating projects",
-      src: "/3.png",
+      dialoge: 'I learned about Logo Maker in through exposure in creating projects',
+      src: '/3.png',
     },
     {
-      title: "Ai Content",
+      title: 'Ai Content',
       index: 3,
-      dialoge: "I learned about Ai Content in through self study",
-      src: "/4.png",
+      dialoge: 'I learned about Ai Content in through self study',
+      src: '/4.png',
     },
-        {
-      title: "Front End",
-      index: 4,
-      dialoge: "I learned about Front End in Bootcamp",
-      src: "/1.png",
-    },
-    {
-      title: "Back End",
-      index: 5,
-      dialoge: "I learned about Back End in Bootcamp",
-      src: "/2.png",
-    },
-    {
-      title: "Logo Maker",
-      index: 6,
-      dialoge: "I learned about Logo Maker in through exposure in creating projects",
-      src: "/3.png",
-    },
-    {
-      title: "Ai Content",
-      index: 7,
-      dialoge: "I learned about Ai Content in through self study",
-      src: "/4.png",
-    },
-    
   ];
 
- 
   return (
-    <>
-      <React.Fragment>
-        <div>
-          <article style={{ backgroundColor: styles.blacky, cursor: 'pointer' }}>
-            <div className="gallery">
-              {numImages.map((image) => (
-                <aside
-                  key={image.index + 1}
-                  style={{ "--i": image.index + 1 }}
-                  onClick={() => handleClickOpen(image)}
-                >
-                  <img src={image.src} alt={image.title} />
-                </aside>
-              ))}
-            </div>
-          </article>
-        </div>
-        <BootstrapDialog
-          onClose={handleClose}
-          aria-labelledby="customized-dialog-title"
-          open={open}
-          sx={{color: "black"}}
-        >
-          <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-            {selectedImage?.title}
-          </DialogTitle>
-          <IconButton
-            aria-label="close"
-            onClick={handleClose}
-            sx={{
-              position: 'absolute',
-              right: 8,
-              top: 8,
-              color: "black",
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
-          <DialogContent dividers>
-            <Typography gutterBottom>{selectedImage?.dialoge}</Typography>
-          </DialogContent>
-          <DialogActions>
-            <Button autoFocus onClick={handleClose}>
-              Close
+  <div className="container" style={{display: "flex", justifyContent:"space-between"}}>
+    {numImages.map((image) => (
+        <Card key={image.index} className="sticky-card" sx={{ maxWidth: 345, boxShadow: '0 4px 0px rgb(0,255,161)', backGround:"rgb(4,13,18)"  }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              style={{ height: 250, width: '100%', backGround:"rgb(4,13,18)"}}
+              image={image.src}
+              alt={image.title}
+            />
+            {/* <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {image.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {image.dialoge}
+              </Typography>
+            </CardContent> */}
+          </CardActionArea>
+          {/* <CardActions>
+            <Button size="small" color="primary">
+              Share
             </Button>
-          </DialogActions>
-        </BootstrapDialog>
-      </React.Fragment>
-      <br/><br/><br/><br/><br/>
-    </>
+          </CardActions> */}
+        </Card>
+      ))}
+    </div>
   );
 }
+
 export default SkillsCarousel;
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,29 +1,33 @@
-import { FloatButton } from 'antd';
+import { BackTop } from 'antd'; // Change FloatButton to BackTop
 import BeginTour from "./../components/BeginTour";
 import SkillsCarousel from "./../components/SkillsCarousel";
 import Footer from "./../components/Footer";
-
+import "../sass/SkillsCarousel.scss"
 
 function Skills() {
-  return<>
-      <div>
-        <div className="body" style={{backgroundColor:"#000F1A", height:"100vh"}}> 
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+  return (
+    <>
+      <div style={{ backgroundColor: "#000F1A" }}>
+        <div className="body" style={{ backgroundColor: "#000F1A", height: "auto" }}>
+          <br /><br /><br /><br /><br /><br />
 
-          <div className="container" style={{ display: 'flex', justifyContent: 'center', marginTop:"1rem" }}>
-            <BeginTour/>
+          <div className="container" style={{ flexDirection: 'row', justifyContent: 'center', marginTop: "1rem" }}>
+            <BeginTour />
           </div>
-          <div style={{backgroundColor:"#000F1A", height:"200vh", marginTop:"-28rem"}}>
-            <SkillsCarousel/>
-          </div>
-          <FloatButton.BackTop />
+          <BackTop /> {/* Change FloatButton.BackTop to BackTop */}
         </div>
-        <div data-aos="fade-up" data-aos-duration="100000" style={{marginTop: "30rem"}}>
-          <Footer/>
+        <div className="sticky-card" data-aos="fade-out" data-aos-duration="100000" style={{ backgroundColor: "#000F1A", flexDirection: "row"}}>
+          <SkillsCarousel />
+        </div>
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+        <div data-aos="fade-up" data-aos-duration="100000" >
+          <Footer />
         </div>
       </div>
-
-  </>;
+    </>
+  );
 }
 
 export default Skills;
